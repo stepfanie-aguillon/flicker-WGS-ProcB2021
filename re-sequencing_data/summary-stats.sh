6ca04bc90431a95ada6d74c33cf61693f79144ea
Stepfanie M Aguillon <38443540+stepfanie-aguillon@users.noreply.github.com>
2020-06-04 14:06:39 -0400

    Update summary-stats.sh

--- re-sequencing_data/summary-stats.sh
+++ re-sequencing_data/summary-stats.sh
@@ -10,20 +10,20 @@
 
 
 ## 25kb windowed FST
-vcftools --vcf RSFL-YSFL_WGS_missing80_mindepth3.vcf.recode.vcf --out RSFL-YSFL_25kb --weir-fst-pop RSFL_indivs.txt --weir-fst-pop YSFL_indivs.txt --fst-window-size 25000 &
+vcftools --vcf RSFL-YSFL_WGS_missing80_mindepth3.vcf --out RSFL-YSFL_25kb --weir-fst-pop RSFL_indivs.txt --weir-fst-pop YSFL_indivs.txt --fst-window-size 25000 &
 ## 50kb windowed FST
-vcftools --vcf RSFL-YSFL_WGS_missing80_mindepth3.vcf.recode.vcf --out RSFL-YSFL_50kb --weir-fst-pop RSFL_indivs.txt --weir-fst-pop YSFL_indivs.txt --fst-window-size 50000 &
+vcftools --vcf RSFL-YSFL_WGS_missing80_mindepth3.vcf --out RSFL-YSFL_50kb --weir-fst-pop RSFL_indivs.txt --weir-fst-pop YSFL_indivs.txt --fst-window-size 50000 &
 ## per-SNP FST
-vcftools --vcf RSFL-YSFL_WGS_missing80_mindepth3.vcf.recode.vcf --out RSFL-YSFL_allSNPs --weir-fst-pop RSFL_indivs.txt --weir-fst-pop YSFL_indivs.txt &
+vcftools --vcf RSFL-YSFL_WGS_missing80_mindepth3.vcf --out RSFL-YSFL_allSNPs --weir-fst-pop RSFL_indivs.txt --weir-fst-pop YSFL_indivs.txt &
 
 ## seq depth
-vcftools --vcf RSFL-YSFL_WGS_missing80_mindepth3.vcf.recode.vcf --out sample_seqdepth --depth &
+vcftools --vcf RSFL-YSFL_WGS_missing80_mindepth3.vcf --out sample_seqdepth --depth &
 
 ## Nucleotide diversity, Tajima's D
-vcftools --vcf RSFL-YSFL_WGS_missing80_mindepth3.vcf.recode.vcf --keep RSFL_indivs.txt --out RSFL_5kbnsl --window-pi 5000 &
-vcftools --vcf RSFL-YSFL_WGS_missing80_mindepth3.vcf.recode.vcf --keep RSFL_indivs.txt --out RSFL_5kbnsl --TajimaD 5000 &
-vcftools --vcf RSFL-YSFL_WGS_missing80_mindepth3.vcf.recode.vcf --keep YSFL_indivs.txt --out YSFL_5kbnsl --window-pi 5000 &
-vcftools --vcf RSFL-YSFL_WGS_missing80_mindepth3.vcf.recode.vcf --keep YSFL_indivs.txt --out YSFL_5kbnsl --TajimaD 5000 &
+vcftools --vcf RSFL-YSFL_WGS_missing80_mindepth3.vcf --keep RSFL_indivs.txt --out RSFL_5kbnsl --window-pi 5000 &
+vcftools --vcf RSFL-YSFL_WGS_missing80_mindepth3.vcf --keep RSFL_indivs.txt --out RSFL_5kbnsl --TajimaD 5000 &
+vcftools --vcf RSFL-YSFL_WGS_missing80_mindepth3.vcf --keep YSFL_indivs.txt --out YSFL_5kbnsl --window-pi 5000 &
+vcftools --vcf RSFL-YSFL_WGS_missing80_mindepth3.vcf --keep YSFL_indivs.txt --out YSFL_5kbnsl --TajimaD 5000 &
 
 ## heterozygosity
-vcftools --vcf RSFL-YSFL_WGS_missing80_mindepth3.vcf.recode.vcf --out sample_het --het &
+vcftools --vcf RSFL-YSFL_WGS_missing80_mindepth3.vcf --out sample_het --het &
