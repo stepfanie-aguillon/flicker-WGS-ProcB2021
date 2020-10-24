89a76489d12af965ff7eb1a7925b34e8e75f4eba
Stepfanie M Aguillon <38443540+stepfanie-aguillon@users.noreply.github.com>
2020-10-24 11:38:38 -0400

    Update 2-summary-stats.sh
    added in command to output VCF of fixed SNPs

--- re-sequencing_data/2-summary-stats.sh
+++ re-sequencing_data/2-summary-stats.sh
@@ -17,10 +17,14 @@ vcftools --vcf RSFL-YSFL_WGS_missing80_mindepth3.vcf --out RSFL-YSFL_50kb --weir
 ## per-SNP FST
 vcftools --vcf RSFL-YSFL_WGS_missing80_mindepth3.vcf --out RSFL-YSFL_allSNPs --weir-fst-pop RSFL_indivs.txt --weir-fst-pop YSFL_indivs.txt &
 
+## fixed SNPs only
+## obtain list of the positions of fixed SNPs from the per-SNP FST file and save as `fixed-snps-list.txt`
+vcftools --vcf RSFL-YSFL_WGS_missing80_mindepth3.vcf --positions fixed-snps-list.txt --recode &
+
 ## seq depth
 vcftools --vcf RSFL-YSFL_WGS_missing80_mindepth3.vcf --out sample_seqdepth --depth &
 
-## Nucleotide diversity, Tajima's D
+## nucleotide diversity, Tajima's D
 vcftools --vcf RSFL-YSFL_WGS_missing80_mindepth3.vcf --keep RSFL_indivs.txt --out RSFL_5kbnsl --window-pi 5000 &
 vcftools --vcf RSFL-YSFL_WGS_missing80_mindepth3.vcf --keep RSFL_indivs.txt --out RSFL_5kbnsl --TajimaD 5000 &
 vcftools --vcf RSFL-YSFL_WGS_missing80_mindepth3.vcf --keep YSFL_indivs.txt --out YSFL_5kbnsl --window-pi 5000 &
