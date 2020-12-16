bdbafc0bfce257f7e505cd44af11b35e0aceebad
Stepfanie M Aguillon <38443540+stepfanie-aguillon@users.noreply.github.com>
2020-12-16 09:08:19 -0500

    Update 2-summary-stats.sh

--- re-sequencing_data/2-summary-stats.sh
+++ re-sequencing_data/2-summary-stats.sh
@@ -24,11 +24,13 @@ vcftools --vcf RSFL-YSFL_WGS_missing80_mindepth3.vcf --positions fixed-snps-list
 ## seq depth
 vcftools --vcf RSFL-YSFL_WGS_missing80_mindepth3.vcf --out sample_seqdepth --depth &
 
+### not in final manuscript
 ## nucleotide diversity, Tajima's D
-vcftools --vcf RSFL-YSFL_WGS_missing80_mindepth3.vcf --keep RSFL_indivs.txt --out RSFL_5kbnsl --window-pi 5000 &
-vcftools --vcf RSFL-YSFL_WGS_missing80_mindepth3.vcf --keep RSFL_indivs.txt --out RSFL_5kbnsl --TajimaD 5000 &
-vcftools --vcf RSFL-YSFL_WGS_missing80_mindepth3.vcf --keep YSFL_indivs.txt --out YSFL_5kbnsl --window-pi 5000 &
-vcftools --vcf RSFL-YSFL_WGS_missing80_mindepth3.vcf --keep YSFL_indivs.txt --out YSFL_5kbnsl --TajimaD 5000 &
+#vcftools --vcf RSFL-YSFL_WGS_missing80_mindepth3.vcf --keep RSFL_indivs.txt --out RSFL_5kbnsl --window-pi 5000 &
+#vcftools --vcf RSFL-YSFL_WGS_missing80_mindepth3.vcf --keep RSFL_indivs.txt --out RSFL_5kbnsl --TajimaD 5000 &
+#vcftools --vcf RSFL-YSFL_WGS_missing80_mindepth3.vcf --keep YSFL_indivs.txt --out YSFL_5kbnsl --window-pi 5000 &
+#vcftools --vcf RSFL-YSFL_WGS_missing80_mindepth3.vcf --keep YSFL_indivs.txt --out YSFL_5kbnsl --TajimaD 5000 &
 
+### not in final manuscript
 ## heterozygosity
-vcftools --vcf RSFL-YSFL_WGS_missing80_mindepth3.vcf --out sample_het --het &
+#vcftools --vcf RSFL-YSFL_WGS_missing80_mindepth3.vcf --out sample_het --het &
