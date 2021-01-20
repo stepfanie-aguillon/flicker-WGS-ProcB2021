6554bb255ba5455d19c68ca56a02fa0fbdf7c904
Stepfanie M Aguillon <38443540+stepfanie-aguillon@users.noreply.github.com>
2021-01-20 13:09:13 -0500

    Update 1-reference_assembly.sh

--- reference_assembly/1-reference_assembly.sh
+++ reference_assembly/1-reference_assembly.sh
@@ -1,4 +1,4 @@
-# This script contains the code to produce the re-sequencing data used in this manuscript:
+# This script contains the code to assemble the reference genome used in this manuscript:
 #
 # Authors: Aguillon SM, Walsh J, Lovette IJ
 # Year: 2021
