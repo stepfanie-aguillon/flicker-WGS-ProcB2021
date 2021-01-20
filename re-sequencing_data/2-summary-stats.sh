2e26cc1976d828ab86e6fc396198fe01fc5cd89b
Stepfanie M Aguillon <38443540+stepfanie-aguillon@users.noreply.github.com>
2021-01-20 13:09:47 -0500

    Update 2-summary-stats.sh

--- re-sequencing_data/2-summary-stats.sh
+++ re-sequencing_data/2-summary-stats.sh
@@ -1,4 +1,4 @@
-# This script contains the code to produce the re-sequencing data used in this manuscript:
+# This script contains the code to obtain vcftools summary stats used in the manuscript:
 #
 # Authors: Aguillon SM, Walsh J, Lovette IJ
 # Year: 2021
